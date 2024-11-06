3aa67fc127f376419ca552003fde1a4353d1d9ea
DhanrajGedam <dhanarajdhanugedam6@gmail.com>
2024-11-06 10:02:46 +0530

    Mobile Application

---
+++ src/main/java/Accenture/App.java
@@ -0,0 +1,11 @@
+package Accenture;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
